37f011e309398a185b2b6dc3d168d8c9b1b5fa03
GannaKov <zlatta2000@gmail.com>
2023-07-27 17:14:32 +0200

    baseTypes

--- src/baseTypes/2.ts
+++ src/baseTypes/2.ts
@@ -1,5 +1,10 @@
-let anything = -20;
-anything = 'Text';
+let anything: unknown = -20; //any also ok
+anything = "Text";
 anything = {};
 
 export {};
+
+//var 2
+let anythingA: number | string | object = -20;
+anythingA = "Text";
+anythingA = {};
--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,45 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+enum DaysOfWeek {
+  Sunday,
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+}
+function isWeekend(day: DaysOfWeek): boolean {
+  if (day === 0 || day === 6) {
+    return true;
+  } else {
+    console.log("no");
+    return false;
+  }
+}
+
+enum DaysOfWeekF {
+  Sunday,
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+}
+type DayStatus = Record<DaysOfWeekF, boolean>;
+const dayStatus: DayStatus = {
+  [DaysOfWeekF.Sunday]: true,
+  [DaysOfWeekF.Monday]: false,
+  [DaysOfWeekF.Tuesday]: false,
+  [DaysOfWeekF.Wednesday]: false,
+  [DaysOfWeekF.Thursday]: false,
+  [DaysOfWeekF.Friday]: false,
+  [DaysOfWeekF.Saturday]: true,
+};
+function isWeekendF(day: DayStatus) {
+  console.log(day);
+}
+console.log(dayStatus);
+// isWeekendF(dayStatus[DaysOfWeekF.Sunday]);
